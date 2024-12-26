955c78da1d8a4d5164b179c9d5fb30cad3b4d100
alsyaaa <142280309+alsyanvma@users.noreply.github.com>
2024-12-26 12:46:38 +0700

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  css: ["bootstrap/dist/css/bootstrap.min.css"],
-  modules: ["@nuxtjs/supabase"],
-  supabase: { redirect: false },
+  css: ['bootstrap/dist/css/bootstrap.min.css'],
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',
+        },
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css', // CDN untuk Bootstrap Icons
+        },
+      ],
+    },
+  },
 });
